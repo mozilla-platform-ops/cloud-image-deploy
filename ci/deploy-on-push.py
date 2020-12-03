d8c336f9e3688a2c0f5f6410c879dec33a5c35f2
Rob Thijssen <rthijssen@gmail.com>
2020-12-03 16:02:38 +0200

    role updates too

--- ci/deploy-on-push.py
+++ ci/deploy-on-push.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 import taskcluster
-from cid import updateClient, updateWorkerPool
+from cid import updateClient, updateRole, updateWorkerPool
 
 
 basePath = os.path.abspath(os.path.dirname(__file__))
@@ -15,18 +15,25 @@ with open(secretsPath, 'r') as secretsStream:
         for environment in deployConfig:
             print('taskcluster {} rootUrl: {}'.format(environment, taskclusterOptions[environment]['rootUrl']))
 
-            # clients
             authClient = taskcluster.Auth(taskclusterOptions[environment])
+            workerManagerClient = taskcluster.WorkerManager(taskclusterOptions[environment])
+
+            # clients
             for clientId in deployConfig[environment]['client']:
                 clientConfigPath = os.path.abspath('{}/../config/{}/client/{}.yml'.format(basePath, environment, clientId))
                 print('- updating client: {}/{} with {}'.format(environment, clientId, clientConfigPath))
                 updateClient(authClient, clientConfigPath, clientId)
+            # roles
+            for roleId in deployConfig[environment]['role']:
+                roleConfigPath = os.path.abspath('{}/../config/{}/role/{}.yml'.format(basePath, environment, roleId))
+                print('- updating role: {}/{} with {}'.format(environment, roleId, roleConfigPath))
+                updateRole(authClient, roleConfigPath, roleId)
 
             # pools
-            workerManagerClient = taskcluster.WorkerManager(taskclusterOptions[environment])
             for domain in deployConfig[environment]['pool']:
                 for pool in deployConfig[environment]['pool'][domain]:
                     poolId = '{}/{}'.format(domain, pool)
                     poolConfigPath = os.path.abspath('{}/../config/{}/pool/{}/{}.yml'.format(basePath, environment, domain, pool))
                     print('- updating pool: {}/{} with {}'.format(environment, poolId, poolConfigPath))
                     updateWorkerPool(workerManagerClient, poolConfigPath, poolId)
+

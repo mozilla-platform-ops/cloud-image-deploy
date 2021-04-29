93721edda470a0e2444a0cddbb26783e59499916
Rob Thijssen <rthijssen@gmail.com>
2021-04-29 17:45:47 +0300

    add auth:sentry for beta workers

--- ci/cloud-image-deploy.py
+++ ci/cloud-image-deploy.py
@@ -80,6 +80,7 @@ createTask(
                 'cargo run --',
                 'deploy',
                 '--worker-pool-pattern "^(relops.*|(gecko|mpd001)-[1-3t]/([bt]-)?win.*)$"',
+                '--role-pattern "^worker-pool:gecko-[1t]/[bt]-win(2012|10-64|7-32)-(beta|gpu-b)$"',
             ]),
         ])
     ],
